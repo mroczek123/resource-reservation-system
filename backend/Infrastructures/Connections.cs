c780ec59acee36ff4d66578ef66f56f243aef13b
dgaweda <32791268+dgaweda@users.noreply.github.com>
2020-11-24 01:30:37 +0100

    REST Client Example: #34 REST Client Example
    Showing some data about repositories and when was last push.

--- backend/Infrastructures/Connections.cs
+++ backend/Infrastructures/Connections.cs
@@ -3,9 +3,11 @@ using System;
 using System.Collections.Generic;  
 using System.Linq;  
 using System.Text;  
-using MySql.Data.MySqlClient;  
+using MySql.Data.MySqlClient;
 using System.Windows.Forms;  
-using System.Data;namespace backend
+using System.Data;
+
+namespace backend
 {  
     class connection  
     {  
--- frontend/Program.cs
+++ frontend/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,14 +14,41 @@ namespace frontend
 {
     public class Program
     {
+        private static readonly HttpClient Client = new HttpClient();
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            var Repositories = await ProcessRepositories();
+            foreach(var repo in Repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.LastPush);
+                Console.WriteLine();
+            }
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
+        }
+
+        private static async Task Task<List<Repository>>ProcessRepositories()
+        {
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            Client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var StreamTask = Client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var Repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await StreamTask);
+
+            var Msg = await StreamTask;
+            Console.Write(Msg);
         }
     }
 }
