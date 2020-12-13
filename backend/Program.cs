149ef909383691093a81c63f2d1e488b4b7bcf53
Patryk.Kirszenstein <thebestilspl@gmail.com>
2020-12-14 00:13:45 +0100

    Basic flow for tabl  (#47)
    * Basic Table flow

--- backend/Program.cs
+++ backend/Program.cs
@@ -15,6 +15,7 @@ namespace backend
     {
         public static void Main(string[] args)
         {
+
             var host = CreateHostBuilder(args).Build();
             host.Run();
         }
---
+++ backend/service/TableService.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Data.SQLite;
+using entity.order;
+
+namespace backend.service
+{
+    public class TableService(SQLiteConnection connection,TableRepo database)
+    {
+        private SQLiteConnection connection{ get; set; }
+        private TableRepo database{ get; set; }
+        
+        public Table CreateTable(Table table)
+        {
+            return database.InsertTableEntity(connection, table);
+        }
+
+        public int DeleteTable(Id id)
+        {
+            return  database.DeleteTableEntity(connection, id);
+        }
+        public Table UpdateTable(Table table)
+        {
+            return  database.UpdateWholeTableEntity(connection, table);
+        }
+        public Table ChangeTableStatus(Id id, Status status)
+        {
+            return  database.UpdateTableEntityStatus(connection, id, status);
+        }
+        
+        public Table GetTable(Id id)
+        {
+            return database.GetTableEntity(connection, id);
+        }
+        public List<Table> GetAllTablesGetTable()
+        {
+            return database.GetAllData(connection);
+        }
+        public List<Table> FindTableByStatus(Status status)
+        {
+           return database.FindByStatus(connection, status);
+        }
+    }
+}
