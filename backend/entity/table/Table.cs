8845b5758482bc39eabe9478caa8b3d9f6080662
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-01-08 01:33:35 +0100

    Table
    - Adding default CRUD to table entity

--- backend/entity/table/Table.cs
+++ backend/entity/table/Table.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace entity.order
 {
     public class Table
--- backend/service/TableService.cs
+++ backend/service/TableService.cs
@@ -1,17 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Linq;
+using backend.entity.utilites;
 using entity.order;
 
 namespace backend.service
 {
     public class TableService
     {
-        private SQLiteConnection connection { get; set; }
+        private DataContext _tables;
 
+        public void Add(Table table)
+        {
+            _tables.Add(table);
+        }
+
+        public void Remove(Table table)
+        {
+            _tables.Remove(table);
+        }
+
+        public void Update(int Id, Table table)
+        {
+            Table TableToEdit = _tables.Tables.ToList().Find(x => x.Id == Id);
+            _tables.Remove(TableToEdit);
+            _tables.Add(table);
+        }
         public IEnumerable<Table> GetAllTablesGetTable()
         {
-            throw new NotImplementedException();
+            return _tables.Tables;
         }
     }
 }
