20df22733f5db4b64d1855a9223150a2ff49b353
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-01-08 02:42:42 +0100

    Update OrderService.cs

--- backend/service/OrderService.cs
+++ backend/service/OrderService.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Linq;
 using backend.entity.utilites;
 using entity.order;
 
@@ -7,11 +9,45 @@ namespace backend.service
 {
     public class OrderService
     {
-        private DataContext context;
-    
-        
-        
-        
+        private DataContext _orders;
+
+        public void Add(Order order)
+        {
+            _orders.Add(order);
+        }
+
+        public void Update(Guid Id, Order order)
+        {
+            Order _order = _orders.OrderSet.ToList()
+                .Find(x => x.Id == Id);
+            
+            _orders.Remove(_order);
+            _orders.Add(order);
+        }
+
+        public Order Get(Guid Id)
+        {
+            return _orders.OrderSet.ToList()
+                .Find(x => x.Id == Id);
+        }
+
+        public IEnumerable<Order> GetAll()
+        {
+            return _orders.OrderSet;
+        }
+
+        public void OrderStatus(Guid Id, Order.status status)
+        {
+            Order _order = _orders.OrderSet.ToList()
+                .Find(x => x.Id == Id);
+
+            _order.Status = status;
+
+            if (status.Equals(Order.status.Done))
+            {
+                _orders.Remove(_order);
+            }
+        }
     }
 
   
