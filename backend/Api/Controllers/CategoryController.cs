55fc0fdc058e3b9ce28104d199704813efc356aa
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-01-17 13:54:58 +0100

    #28 Manage categories
    Added CategoryController + services

---
+++ backend/Api/Controllers/CategoryController.cs
@@ -0,0 +1,47 @@
+﻿using backend.entity.product;
+using backend.service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly CategoryService _categoryService;
+
+        public CategoryController(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet("{categoryId}")]
+        public Category GetOne(Guid categoryId)
+        {
+            return _categoryService.GetOne(categoryId);
+        }
+
+        [HttpGet("All/")]
+        public IEnumerable<Category> GetAll()
+        {
+            return _categoryService.GetAll();
+        }
+
+        [HttpPut("Edit/{categoryId}")]
+        public Category Edit(Guid categoryId, Category category)
+        {
+            _categoryService.Edit(categoryId, category);
+            return category;
+        }
+
+        [HttpPost("Create/{categoryId}")]
+        public ActionResult<Category> AddCategory(Category category)
+        {
+            _categoryService.Add(category);
+            return CreatedAtAction("GetOne", new { id = category.Id }, category);
+        }
+    }
+}
---
+++ backend/service/CategoryService.cs
@@ -0,0 +1,46 @@
+﻿using backend.entity.product;
+using backend.entity.utilites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.service
+{
+    public class CategoryService
+    {
+        private DataContext _category;
+
+        public void Add(Category category)
+        {
+            _category.Add(category);  
+        }
+
+        public void Remove(Guid categoryId)
+        {
+            _category.Remove(categoryId);
+        }
+
+        public Category Edit(Guid categoryId, Category category)
+        {
+            var SelectedCategory = _category.CategorySet
+                .ToList()
+                .Find(c => c.Id == categoryId);
+            SelectedCategory = category;
+            
+            return SelectedCategory;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _category.CategorySet;
+        }
+
+        public Category GetOne(Guid categoryId)
+        {
+            var SelectedCategory = _category.CategorySet.ToList().Find(c => c.Id == categoryId);
+
+            return SelectedCategory;
+        }
+    }
+}
